b0f262656d9d9c9a077f8717f4322009d966207f
t.zemmari <tarik.zemmari@xabec.es>
2021-04-21 18:52:11 +0200

    added user-router logic

---
+++ Routers/user-router.js
@@ -0,0 +1,79 @@
+const router = require("express").Router();
+const userController = require('../Controllers/user-controller');
+
+
+
+router.get('/',async (req,res)=>{
+     
+    try{
+     res.status(200).json(await userController.getAllUsers());
+    }catch(error){
+        res.status(500).send({message:error.message});
+    }
+
+});
+
+router.get('/:id',async (req,res)=>{
+    try{
+    let id = req.params.id;    
+    res.status(200).json(await userController.getUserById(id));
+    }catch(error){
+        res.status(500).send({message:error.message});
+    }
+});
+
+router.get('/',async (req,res)=>{
+    try{
+        let user = await userController.getUserByEmail(email);
+        res.status(200).json(user);
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+});
+
+router.post("/", async(req, res) => {
+    try {
+        const user = await userController.CreateUser(req.body);
+        const status = "Success";
+        res.json({ status, user });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    };
+});
+
+
+
+router.put('/:id',async (req,res)=>{
+    try{
+       
+       let id = req.body.id;
+       let status = 'Success';
+       let user = await userController.updateUser(id,user)
+       res.status(200).json({status,user});
+    }catch(error){
+        res.status(500).send({message:error.message});
+    }
+
+
+
+})
+
+router.delete('/id:',async (req,res)=>{
+    try{
+       res.json(await userController.destroy(id));
+    }catch(error){
+        res.status(500).send({message:error.message});
+    }
+
+
+
+});
+
+
+
+module.exports = router;
+
+
