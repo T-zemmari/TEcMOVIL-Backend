29164f7f0cb1213fb1d401467f56497b71881c32
t.zemmari <tarik.zemmari@xabec.es>
2021-05-01 19:05:51 +0200

    added middlware for user and admin

---
+++ Middleware/Auth.js
@@ -0,0 +1,31 @@
+const login = require('../routers/login-router');
+const jwt = require('jsonwebtoken');
+const secret = 'Tarek es el mejor'
+
+
+
+const authenticate =  (req,res,next)=>{
+
+    try{
+        
+      let token =  req.headers.authorization.split(' ')[1];
+    
+      let payload=  jwt.verify(token,secret);
+      
+      if(payload.userId != req.params.id){
+          throw new Error('No se ha conseguido la verficiacion')
+      }
+      return next();
+
+    }catch(error){
+        res.status(500).json({
+            message: error.message
+        })
+    }
+    
+}
+
+
+
+
+module.exports = authenticate;
