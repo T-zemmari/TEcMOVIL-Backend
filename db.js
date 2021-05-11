707375fa9c9d7fde75d14b75d20f30c20c38006a
t.zemmari <tarik.zemmari@xabec.es>
2021-05-11 21:44:25 +0200

    still fixing the deploy to heroku

--- db.js
+++ db.js
@@ -1,5 +1,24 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
+
+const MONGO_SERVER = process.env.MONGO_SERVER || 'mongodb'
+const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
+const MONGO_PORT = process.env.MONGO_PORT || '27017';
+const MONGO_DBNAME = process.env.MONGO_DBNAME || 'MyProject';
+const MONGO_CLUSTER = process.env.MONGO_CLUSTER || null;
+const MONGO_USER = process.env.MONGO_USER || null;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || null;
+
+const QUERY_STRING = MONGO_USER ?
+
+`${MONGO_SERVER}://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/${MONGO_DBNAME}?retryWrites=true&w=majority`
+
+:
+
+`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}`
+
+
+/*
 const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
 const MONGO_PORT = process.env.MONGO_PORT || '27017';
 const MONGO_DBNAME = process.env.MONGO_DBNAME || 'MyProject';
@@ -10,14 +29,14 @@ const QUERY_STRING = MONGO_USER ?
 `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}`:
 `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}`
 
-const MONGO_URL=process.env.MONGO_URL;
+const MONGO_URL=process.env.MONGO_URL;*/
 
 
 // Connection to DB
 
 
 
-const db = mongoose.connect(`${MONGO_URL}`,{useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connect(QUERY_STRING,{useNewUrlParser: true, useUnifiedTopology: true})
 .then(console.log('Connected to Database'))
 .catch((error) => console.log(error));
 
