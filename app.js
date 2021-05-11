5513ded84d68615d844ebdb568e023288f13d9e4
t.zemmari <tarik.zemmari@xabec.es>
2021-05-11 17:06:54 +0200

    still fixing the deploy , depoly successful ,Application error

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ const router = require('./router');
 const db = require('./db');
 const cors = require('cors')
 const app = express();
-const port = process.env.PORT || '3002'
+const port =3002;
 
 require('crypto').randomBytes(48).toString('hex')
 
 
 //Midelwares
 
-app.use(cors())
+
 app.use(express.json());
-app.use('/public',express.static(`${__dirname}/libs`))
+app.use(cors());
 app.use(router);
+app.use('/public',express.static(`${__dirname}/libs`));
+
 
 
 
 
 db.then(() => {
-    app.listen(port, () => {
-        console.log(`Escuchando en el puerto ${port}`)
-    })
-}).catch(console.log);
+    app.listen(process.env.PORT || port, () => console.log(`Node server running on http://localhost:${port}`));
+}).catch((err) => console.log(err.message));
