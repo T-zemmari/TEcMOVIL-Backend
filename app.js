780347cf014cf66c7a45cb4d8bb852ef115806d0
t.zemmari <tarik.zemmari@xabec.es>
2021-05-11 15:11:19 +0200

    fixing the deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const router = require('./router');
 const db = require('./db');
 const cors = require('cors')
 const app = express();
-const Port = 3002;
+
 require('crypto').randomBytes(48).toString('hex')
 
 
@@ -18,7 +18,7 @@ app.use(router);
 
 
 db.then(() => {
-    app.listen(process.env.PORT || Port, () => {
-        console.log(`Escuchando en el puerto ${Port}`)
+    app.listen(process.env.PORT || 3002, () => {
+        console.log(`Escuchando en el puerto ${3002}`)
     })
 }).catch(console.log);
